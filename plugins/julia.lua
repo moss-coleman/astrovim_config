b56fd8d2dde16b33ca2e63696ded546dc3c40fc0
moss-coleman <mosscoleman@gmail.com>
2024-10-25 15:16:21 +0200

    update to get language server to work

---
+++ plugins/julia.lua
@@ -0,0 +1,17 @@
+return {
+    -- {
+    --   "thibthib18/ros-nvim",
+    --   config = function()
+    --     require("ros-nvim").setup {}
+    --   end,
+    -- },
+    --
+    {
+       'JuliaEditorSupport/julia-vim'
+       -- 'tadachs/ros-nvim',
+       --  config = function()
+       --    require("ros-nvim").setup {only_workspace = true}
+       --  end,
+       --  dependencies = { "nvim-lua/plenary.nvim" },
+    },
+}
