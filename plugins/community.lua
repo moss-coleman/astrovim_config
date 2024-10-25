d42a5f972c40f1ece2d92037573aadb7074f7edf
moss-coleman <mosscoleman@gmail.com>
2024-10-25 15:20:44 +0200

    removed latex community package

--- plugins/community.lua
+++ plugins/community.lua
@@ -6,6 +6,6 @@ return {
 
   -- { import = "astrocommunity.colorscheme.catppuccin" },
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
-  { import = "astrocommunity.markdown-and-latex.vimtex" },
+  -- { import = "astrocommunity.markdown-and-latex.vimtex" },
 }
 
