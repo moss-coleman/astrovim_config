b07a911ed7169c5020272d0d44c312a026251150
moss-coleman <mosscoleman@gmail.com>
2024-12-11 12:57:10 +0100

    updated vimtex plugin

--- plugins/vimtex.lua
+++ plugins/vimtex.lua
@@ -6,16 +6,19 @@ return {
     -- tag = "v2.15", -- uncomment to pin to a specific release
     init = function()
       -- VimTeX configuration goes here, e.g.
-      vim.g.vimtex_view_method = "general"
+      -- vim.g.vimtex_view_method = "general"
       -- vim.g.vimtex_view_method = "zathura"
+      vim.g.vimtex_view_method = "sioyek"
       -- vim.g.vimtex_view_method = "mupdf"
       -- vim.g.vimtex_view_method = "evince"
       -- vim.g.vimtex_view_method = "okular"
       -- Or with a generic interface:
-      let g:vimtex_view_general_viewer = 'okular'
-      let g:vimtex_view_general_options = '--unique file:@pdf\#src:@line@tex'
-      -- vim.g.vimtex_view_general_viewer = 'okular'
-      -- vim.g.vimtex_view_general_options = '--unique file:@pdf\#src:@line@tex'
+      --
+      vim.g.vimtex_view_sioyek_exe = "/opt/sioyek/Sioyek-x86_64.AppImage"
+      -- let g:vimtex_view_general_viewer = 'okular'
+      -- let g:vimtex_view_general_options = '--unique file:@pdf\#src:@line@tex'
+      -- -- vim.g.vimtex_view_general_viewer = 'okular'
+      -- -- vim.g.vimtex_view_general_options = '--unique file:@pdf\#src:@line@tex'
 
     end
 
